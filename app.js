c7b5c579da60606b807f5235b3ce9edb5e485ee4
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-11-30 00:52:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let request = fetch('http://localhost:1337/question');
+    let jsonResponse = '';
+
+
+    request.then(function (response) {
+        jsonResponse = response.json();
+        jsonResponse.then(function (data) {
+            console.log(data)
+            for(let i = 0; i < data.length; i++) {
+                let title_i = JSON.stringify(data[i].title);
+                let description_i = JSON.stringify(data[i].description);
+                let number_i = i + 1;
+                let tag_i = JSON.stringify(data[i].tag);
+
+                document.body.insertAdjacentHTML('beforeEnd',
+                        `<div id="contaner" class="card m-5">
+                                <div id="card-body" class="card-body">
+                                <h5 id="title_card" class="card-title">${number_i}. ${title_i}.</h5>
+                                <a href="#">
+                                <p class="card-text">${description_i}</p>
+                                </a>
+                                <span>${tag_i}</span>
+                            </div>
+                        </div>`);
+            }
+        });
+    });
+//===========================================================================================================================
+// функция создания html тегов принемает параметры (str(tag, nameClass)) для создания элементов
+    function createTagHtml(tagName, nameClass, html, element = document.body){
+        let tag = document.createElement(tagName);
+        tag.className = nameClass;
+        element.append(tag);
+        tag.innerHTML += html;
+    };
+//============================================================================================================================
+function formCreateNewQuestions() {
+    let createTitle = document.getElementById('title_input').value;
+    let createDescription = document.getElementById('desc_input').value;
+    let createTag = document.getElementById('tag_input').value;
+}
+
+document.getElementById('btn_form').onclick =  function(event){
+    event.preventDefault();
+
+    let createTitle = document.getElementById('title_input').value;
+    let createDescription = document.getElementById('desc_input').value;
+    let createTag = document.getElementById('tag_input').value;
+
+    fetch('http://localhost:1337/question', {
+    method: 'POST',
+
+    body: JSON.stringify({
+        title: createTitle,
+        description: createDescription,
+        tag: createTag,
+    })
+}).then(res => {
+    res.json().then(data => console.log(data));
+});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
